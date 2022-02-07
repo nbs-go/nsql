c611846372393d5ff0a23490459d14d02f69fa9c
Saggaf <saggaf.arsyad@gmail.com>
2022-02-07 22:02:38 +0700

    [ADDED] Add Delete query builder

---
+++ pq/query/delete.go
@@ -0,0 +1,48 @@
+package query
+
+import (
+	"fmt"
+	"github.com/nbs-go/nsql/query"
+	opt "github.com/nbs-go/nsql/query/option"
+	"github.com/nbs-go/nsql/schema"
+)
+
+type DeleteBuilder struct {
+	schema *schema.Schema
+	where  query.WhereWriter
+}
+
+func (b *DeleteBuilder) Build(args ...interface{}) string {
+	// Get variable format option
+	opts := opt.EvaluateOptions(args)
+	format, ok := opts.GetVariableFormat()
+	if !ok {
+		// If var format is not defined, then set default to query.NamedVar
+		format = query.BindVar
+	}
+
+	// Set variable format in conditions
+	if b.where == nil {
+		// Set where to id
+		b.where = Equal(b.schema, b.schema.PrimaryKey())
+	}
+
+	// Set format in conditions
+	setUpdateFormat(b.where, b.schema, format)
+
+	// Write where
+	where := b.where.WhereQuery()
+
+	return fmt.Sprintf(`DELETE FROM "%s" WHERE %s`, b.schema.TableName(), where)
+}
+
+func (b *DeleteBuilder) Where(w query.WhereWriter) *DeleteBuilder {
+	b.where = w
+	return b
+}
+
+func Delete(s *schema.Schema) *DeleteBuilder {
+	return &DeleteBuilder{
+		schema: s,
+	}
+}
---
+++ pq/query/delete_test.go
@@ -0,0 +1,39 @@
+package query
+
+import (
+	"github.com/nbs-go/nsql/query"
+	opt "github.com/nbs-go/nsql/query/option"
+	"github.com/nbs-go/nsql/schema"
+	"github.com/nbs-go/nsql/test_utils"
+	"testing"
+)
+
+func TestDelete(t *testing.T) {
+	// Init schema
+	s := schema.New(schema.FromModelRef(new(Transaction)))
+
+	// Test #1
+	test_utils.CompareString(t, "DELETE BY ID",
+		Delete(s).Build(),
+		`DELETE FROM "Transaction" WHERE "id" = ?`)
+
+	// Test #2
+	test_utils.CompareString(t, "DELETE BY MULTIPLE CONDITION",
+		Delete(s).Where(
+			And(
+				Equal(s, "id"),
+				Equal(s, "version"),
+			),
+		).Build(),
+		`DELETE FROM "Transaction" WHERE "id" = ? AND "version" = ?`)
+
+	// Test #3
+	test_utils.CompareString(t, "DELETE BY MULTIPLE CONDITION (NAMED VAR)",
+		Delete(s).Where(
+			And(
+				Equal(s, "id"),
+				Equal(s, "version"),
+			),
+		).Build(opt.VariableFormat(query.NamedVar)),
+		`DELETE FROM "Transaction" WHERE "id" = :id AND "version" = :version`)
+}
